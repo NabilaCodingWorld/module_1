f9f22e344e8ec5f4826d29c1c4239e376fb89634
NabilaWorld <mnabila30163@gmail.com>
2024-05-02 11:26:34 +0600

    first commit

---
+++ module_1/module1/src/1.4.ts
@@ -0,0 +1,33 @@
+// Basic Data Type
+
+// string
+// explicit vabe data define kora
+let firstName: string = "Nabila";
+
+// number
+let roll: number = 123;
+
+// boolean
+let isAdmin: boolean = true;
+
+// undefined
+let x: undefined = undefined;
+
+// null
+let y: null = null;
+
+let d: number;
+
+d = 123;
+
+// Array
+
+let friends: string[] = ["Nabila", "Nazmul"];
+friends.push("Raha");
+
+let eligibleRollList: number[] = [1, 3];
+
+// tuple --> array --> order --> type of values
+let ageName: [number, string] = [50, "Mr. X"];
+
+ageName[0] = 10;
